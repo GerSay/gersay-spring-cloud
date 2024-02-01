a726a881523c0fcfd5a732da1cc2b9e329d83d44
GerSay <kryt.master@yandex.ru>
2024-02-02 01:51:27 +0300

    0.0.2 Create demo-eureka-client module         Add:              - "/test" - rest controller: TestController         Update:              - pom.xml

---
+++ demo-eureka-client/src/test/java/ru/spring/cloud/micro/demo/demoeurekaclient/DemoEurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spring.cloud.micro.demo.demoeurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEurekaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
